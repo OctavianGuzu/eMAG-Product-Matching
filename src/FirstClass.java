92ce06abefc7ce84e8aca42d7ce9aa389df2dd48
Octavian Guzu <octavian.guzu@gmail.com>
2016-12-08 22:32:19 +0200

    First class created in the right src directory.

---
+++ src/FirstClass.java
@@ -0,0 +1,10 @@
+package src;
+
+/**
+ * Created by octavian.guzu on 12/8/2016.
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("this is just a test");
+    }
+}
