21de65c9d5a9555f10e77a1c321213fe49f90a7c
Andrei-Nicolae Damian <andreidamian0612@gmail.com>
2016-12-08 22:34:19 +0200

    Added a small change for testing.

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -6,5 +6,6 @@ package src;
 public class FirstClass {
     public static void main(String[] args) {
         System.out.println("this is just a test");
+        System.out.println("this is the second test");
     }
 }
