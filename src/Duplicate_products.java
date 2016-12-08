ddba5c16134b38537e7300edd87041333a5f87be
Octavian Guzu <octavian.guzu@gmail.com>
2016-12-09 01:25:57 +0200

    Implemented the class used for reading all the data from the products_to_be_added file.

--- src/Duplicate_products.java
+++ src/Duplicate_products.java
@@ -1,8 +1,39 @@
 package src;
 
+import com.sun.xml.internal.bind.v2.model.core.ID;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
 /**
  * Created by octavian.guzu on 12/9/2016.
  */
 public class Duplicate_products {
+    Vector products=new Vector();
+    Vector IDs=new Vector();
 
+    public static void main(String args[]) throws IOException {
+        Duplicate_products obj=new Duplicate_products();
+        obj.readFile("produse_de_adaugat.csv");
+    }
+
+    public void readFile(String file) throws IOException {
+        BufferedReader br=new BufferedReader(new FileReader(file));
+        StringTokenizer stringToken;
+        String line;
+        line=br.readLine();
+        while(line != null) {
+            stringToken=new StringTokenizer(line,";");
+            line=stringToken.nextToken();
+            IDs.add(line);
+            line=stringToken.nextToken();
+            products.add(line);
+            line=br.readLine();
+        }
+        System.out.println(products);
+    }
 }
