bfa2d0f2547159f8f56f589fbf833ce59ef6a567
Octavian Guzu <octavian.guzu@gmail.com>
2016-12-08 22:29:03 +0200

    Deleted some random files.

--- src/src/FirstClass.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by octavian.guzu on 12/8/2016.
- */
-public class FirstClass {
-    public static void main(String[] args) {
-        System.out.println("dama");
-        System.out.println("Octavian");
-    }
-}
